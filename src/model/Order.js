d7984d9d4c5dcf252709b5f12d51c881cfa8cab3
Gennady Polakov <gennadypolakov@gmail.com>
2021-08-08 17:43:25 +0400

    обработка ордеров

--- src/model/Order.js
+++ src/model/Order.js
@@ -1,15 +1,9 @@
 import {priceLine} from '../config';
 import {LineStyle} from 'lightweight-charts';
-import {ASK, ASK_ORDER_COLOR, BID, BID_ORDER_COLOR, ORDER_COLOR} from '../constants';
-
-const orderColors = {
-  [ASK]: ASK_ORDER_COLOR,
-  [BID]: BID_ORDER_COLOR,
-};
+import {ASK, ORDER_COLOR} from '../constants';
 
 export class Order {
 
-  bestPriceSubscription;
   bestPrice;
   line;
   orderBook;
@@ -20,13 +14,7 @@ export class Order {
   updatedAt;
   volume;
   side; // ask | bid
-  orderBookSubscription;
-  config$;
-  configSubscription;
   checkId;
-
-  prev;
-  next;
 
   constructor(side, price, volume, orderBook) {
     this.orderBook = orderBook;
@@ -57,39 +45,6 @@ export class Order {
     return false;
   };
 
-  // next = (payload) => {
-  //   this.bestPrice = payload.bestPrice?.[this.side];
-  //   if (payload[this.side] && this.price in payload[this.side]) {
-  //     this.volume = payload[this.side][this.price];
-  //   }
-  //   this.check();
-  // };
-
-  // onOrderBookOld = ({action, payload}) => {
-  //   if (action && this[action]) {
-  //     this[action](payload);
-  //   }
-  // }
-
-  onOrderBook = (orderBook) => {
-    if (orderBook) {
-      if (this.side && this.price && this.price in orderBook[this.side]) {
-        this.volume = orderBook[this.side][this.price];
-        this.check();
-      }
-    }
-  }
-
-  configStream = (config) => {
-    if (config?.action === this.ticker.name) {
-      if (config.type === 'destroy') {
-
-      } else {
-        this.check();
-      }
-    }
-  };
-
   check = (second = false) => {
     if (!this.volume) {
       this.removeLine();
@@ -99,10 +54,12 @@ export class Order {
         const {averageVolume, isTimeout} = this.ticker || {};
         if (this.volume >= averageVolume * minOrderPercentage) {
           if (second) {
+            this.removeLine();
             this.createLine();
             if (!isTimeout) this.ticker?.state?.events$?.next(this);
           } else {
-            this.updateLine();
+            this.removeLine();
+            this.createLine();
             if (orderTimeout) {
               this.timeoutId = setTimeout(() => {
                 this.check(true);
@@ -118,26 +75,6 @@ export class Order {
     }
   };
 
-  checkVolumeOnPriceRange = (endPrice) => {
-    this.checkId = this.orderBook.checkId;
-    if (!endPrice) {
-      const bestPrice = this.orderBook.best[this.side].price;
-      const {priceDistance} = this.ticker?.config || {};
-      if (bestPrice && priceDistance) {
-        endPrice = this.side === ASK ? bestPrice + bestPrice * priceDistance : bestPrice - bestPrice * priceDistance;
-      }
-    }
-    if (endPrice) {
-      const delta = this.side === ASK ? endPrice - this.price : this.price - endPrice;
-      if (delta > 0) {
-        this.check();
-        if (this.next) {
-          this.next.checkVolumeOnPriceRange(endPrice);
-        }
-      }
-    }
-  };
-
   checkLine = (checkId) => {
     if (checkId) {
       this.checkId = checkId;
@@ -147,48 +84,9 @@ export class Order {
     }
   }
 
-  checkBestPrice = () => {
-    const bestPrice = this.orderBook.bestPrice[this.side];
-    if (bestPrice) {
-      const delta = this.side === ASK ? this.price - bestPrice : bestPrice - this.price;
-      if (delta < 0) {
-        if (this.next) {
-          this.orderBook.best[this.side] = this.next;
-          this.next?.checkBestPrice();
-        }
-        this.remove();
-      }
-    }
-  };
-
   update = (volume) => {
       this.volume = volume;
       this.check();
-  };
-
-  updateLinks = (order) => {
-    const price = order.price
-    if (price) {
-      const delta = this.side === ASK ? price - this.price : this.price - price;
-      if (delta > 0) {
-        if (this.next) {
-          this.next.updateLinks(order);
-        } else {
-          this.next = order;
-          this.next.prev = this;
-        }
-      } else if (delta < 0) {
-        if (this.prev) {
-          this.prev.next = order;
-          order.prev = this.prev;
-          order.next = this;
-          this.prev = order;
-        } else {
-          this.prev = order;
-          order.next = this;
-        }
-      }
-    }
   };
 
   updateLine = () => {
@@ -198,7 +96,6 @@ export class Order {
       });
     }
   };
-
 
   createLine = () => {
     if (
@@ -226,42 +123,12 @@ export class Order {
 
   };
 
-  removePrev = () => {
-    if (this.prev) {
-      this.prev.remove({prev: true, updateLinks: false});
-      delete this.prev;
-    }
-  }
-
-  remove = ({next = false, prev = false, updateLinks = true} = {}) => {
+  remove = () => {
     if (this.removeTimeoutId) clearTimeout(this.removeTimeoutId);
     this.removeLine();
     if (this.side && this.price && this.orderBook?.[this.side] && this.price in this.orderBook[this.side]) {
       delete this.orderBook[this.side][this.price];
     }
-    if (updateLinks) {
-      if (this.next) {
-        if (this.prev) {
-          this.prev.next = this.next;
-          this.next.prev = this.prev;
-        } else {
-          delete this.next.prev;
-        }
-      } else {
-        if (this.prev) {
-          delete this.prev.next;
-        }
-      }
-    }
-    if (next && this.next) {
-      this.next.remove({next, updateLinks: false});
-      delete this.prev;
-    }
-    if (prev && this.prev) {
-      this.prev.remove({prev, updateLinks: false});
-      delete this.next;
-    }
-
     const {state, name} = this.ticker || {};
     if (state && name && state.events?.[name]?.order?.[this.price]) {
       delete state.events[name].order[this.price];
--- src/model/OrderBook.js
+++ src/model/OrderBook.js
@@ -1,35 +1,27 @@
-import {BehaviorSubject, Subject} from 'rxjs';
-import {axiosSpot, getSymbolOrderBook} from '../api';
+import {getSymbolOrderBook} from '../api';
 import {Order} from './Order';
 import {ASK, BID} from '../constants';
-import {apiTimeout, minOrderPercentage} from '../config';
+import {apiTimeout} from '../config';
 
 export class OrderBook {
 
   ask;
   bid;
   bestPrice = {ask: null, bid: null};
-  bestPrice$ = new Subject();
   best = {ask: null, bid: null};
-  bestPriceStream;
   isTimeout;
   lastUpdateId;
-  lastUpdated;
   bestPriceLastUpdateTime = Date.now();
   lines = {ask: {}, bid: {}};
   name;
   orderStream = [];
   orders;
-  orderBook$ = new Subject();
   series;
   state;
-  orderBookStream;
   synced = false;
   ticker;
   U;
   u;
-  orderBookSubscription;
-  bestPriceSubscription;
   checkId;
 
   constructor(ticker) {
@@ -37,76 +29,11 @@ export class OrderBook {
     this.name = ticker?.name;
     this.state = ticker?.state;
     this.series = ticker?.series;
-    // if (this.name) this.createStreams();
-    // this.orderBookSubscription = this.ticker?.stream$?.orderBook?.subscribe(this.onOrderBookMessage);
-    // this.bestPriceSubscription = this.ticker?.stream$?.bestPrice?.subscribe(this.onBestPrice);
     this.ticker?.config?.config$?.subscribe(this.checkOrders);
   }
 
-  // closeStreams = () => {
-  //   this.closeBestPriceStream();
-  //   this.closeOrderBookStream();
-  // }
-
-  // convertStreamValue = (data, side) => {
-  //   let converted = {};
-  //   if (data?.length) {
-  //     converted = data.reduce((acc, current) => {
-  //       const price = Number(current[0]);
-  //       const volume = Number(current[1]);
-  //       if (!this[side]?.[price]) {
-  //         this[side][price] = new Order(side, price, volume, this);
-  //       }
-  //       acc[price] = volume;
-  //       return acc;
-  //     }, converted);
-  //   }
-  //   return converted;
-  // };
-
-  // createStreams = () => {
-  //   this.createOrderBookStream();
-  //   this.createBestPriceStream();
-  // };
-
-  // createOrderBookStream = () => {
-  //   this.orderBookStream = new WebSocket(`wss://stream.binance.com:9443/ws/${this.name.toLowerCase()}@depth@1000ms`);
-  //   this.orderBookStream.addEventListener('message', this.onOrderBookMessage);
-  //   this.orderBookStream.addEventListener('error', this.onOrderBookError);
-  // };
-
-  // closeOrderBookStream = () => {
-  //   this.orderBookStream?.removeEventListener('message', this.onOrderBookMessage);
-  //   this.orderBookStream?.removeEventListener('error', this.onOrderBookError);
-  //   this.orderBookStream?.close();
-  // };
-
-  // onOrderBookError = () => {
-  //   this.closeOrderBookStream();
-  //   setTimeout(this.createOrderBookStream, 5000);
-  // };
-
-  // createBestPriceStream = () => {
-  //   this.bestPriceStream = new WebSocket(`wss://stream.binance.com:9443/ws/${this.name.toLowerCase()}@bookTicker`);
-  //   this.bestPriceStream.addEventListener('message', this.onBestPrice);
-  //   this.bestPriceStream.addEventListener('error', this.onBestPriceError);
-  // };
-
-  // closeBestPriceStream = () => {
-  //   this.bestPriceStream?.removeEventListener('message', this.onBestPrice);
-  //   this.bestPriceStream?.removeEventListener('error', this.onBestPriceError);
-  //   this.bestPriceStream?.close();
-  // };
-
-  // onBestPriceError = () => {
-  //   this.closeBestPriceStream();
-  //   setTimeout(this.createBestPriceStream, 5000);
-  // };
-
   checkOrders = () => {
     this.checkId = Symbol();
-    // this.best.ask?.checkVolumeOnPriceRange();
-    // this.best.bid?.checkVolumeOnPriceRange();
     const priceDistance = this.ticker?.config?.priceDistance || 0;
     let bestPrice = this.bestPrice.ask;
     let endPrice = bestPrice + bestPrice * priceDistance;
@@ -159,28 +86,14 @@ export class OrderBook {
         if (this.ask && this.bestPrice.ask) {
           const prices = Object.keys(this.ask).map((p) => Number(p)).filter((p) => p < this.bestPrice.ask);
           prices.forEach((p) => {
-            this.ask[p].remove({updateLinks: false});
+            this.ask[p].remove();
           });
-          // const bestAsk = this.ask[this.bestPrice.ask];
-          // if (bestAsk) {
-          //   bestAsk.removePrev();
-          //   this.best.ask = bestAsk;
-          // } else if (this.best.ask) {
-          //   this.best.ask.checkBestPrice();
-          // }
         }
         if (this.bid && this.bestPrice.bid) {
           const prices = Object.keys(this.bid).map((p) => Number(p)).filter((p) => p > this.bestPrice.bid);
           prices.forEach((p) => {
-            this.bid[p].remove({updateLinks: false});
+            this.bid[p].remove();
           });
-          // const bestBid = this.bid[this.bestPrice.bid];
-          // if (bestBid) {
-          //   bestBid.removePrev();
-          //   this.best.bid = bestBid;
-          // } else if (this.best.bid) {
-          //   this.best.bid.checkBestPrice();
-          // }
         }
       }
     }
@@ -193,16 +106,6 @@ export class OrderBook {
     }, (this.ticker?.config?.notificationTimeout || 5) * 1000 * 60);
   };
 
-  setBestPrice = (side) => {
-    const cb = {
-      ask: (a, b) => a - b,
-      bid: (a, b) => b - a
-    };
-    this.bestPrice[side] = Object.keys(this[side])
-      .map((p) => Number(p))
-      .sort(cb[side])[0];
-  };
-
   setOrderBook = () => {
     if (this.state.apiTimeout) {
       setTimeout(this.setOrderBook, 1000 * 60 * apiTimeout + 100);
@@ -210,40 +113,27 @@ export class OrderBook {
       getSymbolOrderBook(this.name, 100)
         .then((data) => {
           if (data) {
-            let prev;
-            this.ask = data.asks?.reduce((acc, o, i) => {
+            this.ask = {};
+            data.asks?.forEach((o) => {
               const price = Number(o[0]);
               const volume = Number(o[1]);
-              const order = new Order(ASK, price, volume, this);
-              if (i === 0) this.best.ask = order;
-              if (prev) {
-                order.prev = prev;
-                prev.next = order;
+              if (this.ask[price]) {
+                this.ask[price].update(volume);
+              } else if (volume) {
+                this.ask[price] = new Order(ASK, price, volume, this);
               }
-              prev = order;
-              acc[price] = order;
-              return acc;
-            }, {}) || {};
-            prev = null;
-            this.bid = data.bids?.reduce((acc, o, i) => {
+            });
+            this.bid = {};
+            data.bids?.forEach((o) => {
               const price = Number(o[0]);
               const volume = Number(o[1]);
-              const order = new Order(BID, price, volume, this);
-              if (i === 0) this.best.bid = order;
-              if (prev) {
-                order.prev = prev;
-                prev.next = order;
+              if (this.bid[price]) {
+                this.bid[price].update(volume);
+              } else if (volume) {
+                this.bid[price] = new Order(BID, price, volume, this);
               }
-              prev = order;
-              acc[price] = order;
-              return acc;
-            }, {}) || {};
-            // this.setBestPrice(ASK);
-            // this.setBestPrice(BID);
+            });
             this.lastUpdateId = data.lastUpdateId;
-            // const {bestPrice} = this;
-            // const {ask, bid} = this;
-            // this.orderBook$.next({ask, bid});
             this.syncOrderBook();
           }
         })
@@ -286,7 +176,7 @@ export class OrderBook {
       update?.a?.forEach((order) => {
         const price = Number(order[0]);
         const volume = Number(order[1]);
-        if (price in this.ask) {
+        if (this.ask[price]) {
           this.ask[price].update(volume);
         } else if (volume) {
           this.ask[price] = new Order(ASK, price, volume, this);
@@ -295,7 +185,7 @@ export class OrderBook {
       update?.b?.forEach((order) => {
         const price = Number(order[0]);
         const volume = Number(order[1]);
-        if (price in this.bid) {
+        if (this.bid[price]) {
           this.bid[price].update(volume);
         } else if (volume) {
           this.bid[price] = new Order(BID, price, volume, this);
